193b60dbca25464dc1d049e1005abe45647f3628
o.saevskii <oleg@saevskii.ru>
2023-03-19 23:30:36 +0300

    Добавлены комментарии к методу перекрещивания

--- программа/PPPI/PPPI/BitmapOverlayModule.cs
+++ программа/PPPI/PPPI/BitmapOverlayModule.cs
@@ -2,6 +2,11 @@
 {
     public class BitmapOverlayModule
     {
+        /// <summary>
+        /// Перекрещивает изображения между собой
+        /// </summary>
+        /// <param name="img1"></param>
+        /// <param name="img2"></param>
         public void Cross(Bitmap img1, Bitmap img2)
         {
 
