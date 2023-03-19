8439de10df1ff08e033e6ea62341468b0c911daa
o.saevskii <oleg@saevskii.ru>
2023-03-19 20:00:51 +0300

    Добавлен класс Channel

--- программа/PPPI/PPPI/ChannelModule.cs
+++ программа/PPPI/PPPI/ChannelModule.cs
@@ -1,6 +1,13 @@
 ﻿namespace PPPI
 {
+    public class Channel
+    {
+        public string Name { get; set; }
+        public object[] Params { get; set; }
+    }
+
     internal class ChannelModule
     {
+        
     }
 }
