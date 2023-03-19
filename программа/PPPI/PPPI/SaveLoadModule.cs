2acea0f11e83914068d63f7b4b88bc8afe93e612
o.saevskii <oleg@saevskii.ru>
2023-03-19 21:08:57 +0300

    Добавлены комментарии к методам

--- программа/PPPI/PPPI/SaveLoadModule.cs
+++ программа/PPPI/PPPI/SaveLoadModule.cs
@@ -2,11 +2,19 @@
 {
     public class SaveLoadModule
     {
+        /// <summary>
+        /// Сохраняет изображение
+        /// </summary>
+        /// <param name="img"></param>
         public void Save(Bitmap img)
         {
 
         }
 
+        /// <summary>
+        /// Загружает изображение
+        /// </summary>
+        /// <param name="img"></param>
         public void Load(Bitmap img)
         {
 
