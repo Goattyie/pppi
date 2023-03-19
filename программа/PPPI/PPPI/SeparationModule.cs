6c1b13cf90cc5fa9747a9aadb2beeac0a82bd285
o.saevskii <oleg@saevskii.ru>
2023-03-19 20:09:42 +0300

    Добавлен метод выделения

--- программа/PPPI/PPPI/SeparationModule.cs
+++ программа/PPPI/PPPI/SeparationModule.cs
@@ -1,6 +1,12 @@
-﻿namespace PPPI
+﻿using System.Drawing.Drawing2D;
+
+namespace PPPI
 {
     internal class SeparationModule
     {
+        public void Separate(Bitmap img, CoordinateSpace[] coordinate)
+        {
+
+        }
     }
 }
