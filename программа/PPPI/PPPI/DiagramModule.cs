23a905aa5cb2739d8dc5e06edd99ab2f2779bccd
o.saevskii <oleg@saevskii.ru>
2023-03-19 20:29:20 +0300

    Изменен список параметров в методах

--- программа/PPPI/PPPI/DiagramModule.cs
+++ программа/PPPI/PPPI/DiagramModule.cs
@@ -1,23 +1,25 @@
-﻿namespace PPPI
+﻿using System.Drawing.Drawing2D;
+
+namespace PPPI
 {
     public class DiagramModule
     {
-        public void CreateGistogram()
+        public void CreateGistogram(Bitmap img, CoordinateSpace coordinateSpace)
         {
 
         }
 
-        public void RemoveGistogram()
+        public void RemoveGistogram(Bitmap img, CoordinateSpace coordinateSpace)
         {
 
         }
 
-        public void CreateСurve()
+        public void CreateСurve(Bitmap img, CoordinateSpace coordinateSpace)
         {
 
         }
 
-        public void RemoveСurve()
+        public void RemoveСurve(Bitmap img, CoordinateSpace coordinateSpace)
         {
 
         }
