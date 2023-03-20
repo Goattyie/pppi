88f8f27b163ca8fcb25158a508d2445de1c6ba03
o.saevskii <oleg@saevskii.ru>
2023-03-20 18:35:07 +0300

    Добавлено описание ChannelModule

--- программа/PPPI/PPPI/ChannelModule.cs
+++ программа/PPPI/PPPI/ChannelModule.cs
@@ -6,6 +6,9 @@
         public object[] Params { get; set; }
     }
 
+    /// <summary>
+    /// Модуль для работы с каналами
+    /// </summary>
     internal class ChannelModule
     {
         public void Add(Bitmap img, Channel channel)
