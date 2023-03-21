8f8867293b0b94610ac395fb24858a8d48ac826e
o.saevskii <oleg@saevskii.ru>
2023-03-21 19:54:05 +0300

    Добавлены hta и chm файлы

--- программа/PPPI/PPPI/StudyModule.cs
+++ программа/PPPI/PPPI/StudyModule.cs
@@ -1,22 +1,46 @@
 ﻿namespace PPPI
 {
+    /**
+    * Справка
+    *
+    * Модуль обучения работы в программе
+    * Содержит справку с гайдами.
+    *
+    * @author      Oleg Saevskii
+    * @version     1.0.0
+    * @copyright   MIT License
+    */
     public class StudyModule
     {
+        /**
+        * Метод получения справки
+        */
         public void GetBook()
         {
 
         }
 
+        /**
+        * Метод перехода по ссылке
+        * @param    string  link    Ссылка
+        */
         public void OpenLink(string link)
         {
 
         }
 
+        /**
+        * Метод открытия видео
+        * @param    string  link    Ссылка
+        */
         public void OpenVideo(string link)
         {
 
         }
 
+        /**
+        * Метод закрытия справки
+        */
         public void CloseBook()
         {
 
